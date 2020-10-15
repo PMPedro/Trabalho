edd4979d1fccf98f82905db3209d6b5d7cd0597b
Pedro Martins <61905272+PedroBBC@users.noreply.github.com>
2020-10-15 22:18:58 +0100

    çnknhuiun

--- app/src/main/java/com/example/healthyabj/loginnumtele.kt
+++ app/src/main/java/com/example/healthyabj/loginnumtele.kt
@@ -2,17 +2,62 @@ package com.example.healthyabj
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.android.synthetic.main.loginnumtele.*
+import java.util.concurrent.TimeUnit
 
 class loginnumtele : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
+    lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loginnumtele)
         auth = FirebaseAuth.getInstance()
 
+
+
+
+        fun VerifyCallbacks () {
+        callbacks = object :PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onVerificationFailed(p0: FirebaseException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                super.onCodeSent(p0, p1)
+            }
+        }
+        }
+
+        fun Verificar() {
+            VerifyCallbacks()
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            numteleloginNumTele.text.toString(), // Phone number to verify
+            60, // Timeout duration
+            TimeUnit.SECONDS, // Unit of timeout
+            this, // Activity (for callback binding)
+            callbacks) // OnVerificationStateChangedCallbacks
+        }
+
+        loginnumteleVerificar.setOnClickListener {
+                Verificar()
+        }
+
+
+
     }
 }
+
+
+/*
+
+ */
