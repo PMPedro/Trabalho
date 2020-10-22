c98e7ac8581b5f645986f5ca447edd050d6b32c6
Pedro Martins <61905272+PedroBBC@users.noreply.github.com>
2020-10-22 22:50:14 +0100

    çnknhuiun

--- app/src/main/java/com/example/healthyabj/ChatActivity.kt
+++ app/src/main/java/com/example/healthyabj/ChatActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.android.synthetic.main.chatopage.*
 
 class ChatActivity : AppCompatActivity() {
 
@@ -14,6 +15,15 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chatopage)
         auth = FirebaseAuth.getInstance()
+
+
+        chatpageSendMessage.setOnClickListener {
+            chatpageMessage.text.toString() == ""
+
+
+        }
+
+
 
 
 
