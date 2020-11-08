feefd776ef8cec0a73c5a696b1f02916796bb4cf
goncaloSoares109 <goncalonmsoares@gmail.com>
2020-11-08 17:54:44 +0000

    recyclerview show users list

--- app/src/main/java/com/example/healthyabj/NewMessageActivity.kt
+++ app/src/main/java/com/example/healthyabj/NewMessageActivity.kt
@@ -2,17 +2,65 @@ package com.example.healthyabj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter_LifecycleAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
-
+import kotlinx.android.synthetic.main.user_row_new_message.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.util.ArrayList
+import kotlin.coroutines.CoroutineContext
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 class NewMessageActivity : AppCompatActivity() {
+    lateinit var name: User
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+        supportActionBar?.title = "Select User"
 
-        supportActionBar?.title="Select User"
+       val query: CollectionReference = db.collection("User")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query,User::class.java)
+            .setLifecycleOwner(this).build()
+        val adapter = object : FirestoreRecyclerAdapter<User, UserViewHolder>(options){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+             val view:View = LayoutInflater.from(this@NewMessageActivity).inflate(R.layout.user_row_new_message, parent,false)
+                return UserViewHolder(view)
+            }
 
-        recyclerview_newmessage.adapter
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+               val usName: TextView = holder.itemView.findViewById(R.id.usernames)
+                usName.text = model.name
+            }
+
+        }
+        recyclerview_newmessage.adapter = adapter
+        recyclerview_newmessage.layoutManager = LinearLayoutManager(this)
+
 
     }
 }
--- app/src/main/java/com/example/healthyabj/User.kt
+++ app/src/main/java/com/example/healthyabj/User.kt
@@ -4,11 +4,13 @@ class User {
 
     lateinit var name:String
     lateinit var email:String
+    lateinit var uid:String
     data class User(
         val uid: String,
         val email: String,
-        val name: String,
-        val password: String
+        val name: String="",
+        val passwnameord: String
+
 
 
     )
