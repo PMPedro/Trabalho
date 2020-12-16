057556474e1359d3208a73ec7b700cf3519cd51f
Pedro Martins <61905272+PMPedro@users.noreply.github.com>
2020-12-16 19:18:25 +0000

    date picker

--- app/src/main/java/com/example/healthyabj/ProxConsultasActivitie.kt
+++ app/src/main/java/com/example/healthyabj/ProxConsultasActivitie.kt
@@ -1,9 +1,12 @@
 package com.example.healthyabj
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.healthyabj.MEDICOS.Medicos_Home
@@ -12,22 +15,45 @@ import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.minhasconsultasuser.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ProxConsultasActivitie : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.minhasconsultasuser)
         auth = FirebaseAuth.getInstance()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         minhasconsultasuserbtHome.setOnClickListener {
             val db = FirebaseFirestore.getInstance()
             val uid = auth.currentUser!!.email
-
 
 
             db.collection("User")
@@ -44,7 +70,7 @@ class ProxConsultasActivitie : AppCompatActivity() {
                             if(cenas == "0"){
 
                                 //Toast.makeText(this, tipo, Toast.LENGTH_SHORT).show()
-                                startActivity(Intent(this,HomePageActivity::class.java))
+                                startActivity(Intent(this, HomePageActivity::class.java))
                             }
 
                             if(cenas == "1") {
@@ -57,49 +83,135 @@ class ProxConsultasActivitie : AppCompatActivity() {
         }
 
 
+
+
+
         val db = FirebaseFirestore.getInstance()
+//
+//
+//        db.collection("Consultas").orderBy("DiaConsulta", Query.Direction.ASCENDING)
+//            .get()
+//            .addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//
+//                    val result: StringBuffer = StringBuffer()
+//
+//                    for (document in task.result) {
+//                        var DC : String
+//                        var HC : String
+//                        var NM : String
+//                        var NP : String
+//
+//                            DC = result.append(document.data.getValue("DiaConsulta")).toString()
+//                            NP = result.append(document.data.getValue("EmaiPaciente")).toString()
+//                            NM = result.append(document.data.getValue("EmailMedico")).toString()
+//                            HC = result.append(document.data.getValue("HoraConsulta")).toString()
+//
+//
+//
+//                        var todoList = mutableListOf(Consultas(DC, NP, NM, HC))
+//
+//
+//
+//
+//                        val adapter = MinhasConsultasAdapter(todoList)
+//                        rvTODO.adapter = adapter
+//                        rvTODO.layoutManager = LinearLayoutManager(this)
+//
+//
+//
+//
+//                    }
+//                } else {
+//                    Log.w(ChatLogActivity.TAG, "Error getting documents.", task.exception)
+//                }
+//            }
 
 
-        db.collection("Consultas").orderBy("DiaConsulta", Query.Direction.ASCENDING)
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-
-                    val result: StringBuffer = StringBuffer()
-
-                    for (document in task.result) {
-                        var DC : String
-                        var HC : String
-                        var NM : String
-                        var NP : String
-
-                            DC = result.append(document.data.getValue("DiaConsulta")).toString()
-                            NP = result.append(document.data.getValue("EmaiPaciente")).toString()
-                            NM = result.append(document.data.getValue("EmailMedico")).toString()
-                            HC = result.append(document.data.getValue("HoraConsulta")).toString()
 
 
 
-                        var todoList = mutableListOf(   Consultas(DC, NP, NM, HC)  )
+fun ReceDia() {
+        val fromId= FirebaseAuth.getInstance().uid
+
+        db.collection("Consultas")
+            .orderBy("DiaConsulta", Query.Direction.ASCENDING)
+            .addSnapshotListener { value, e ->
+
+                if (e != null) {
+                    Log.w(ChatLogActivity.TAG, "Listen failed.", e)
+                    return@addSnapshotListener
+                }
+
+
+
+                for (doc in value!!) {
+
+                    var DC : String
+                    var HC : String
+                    var NM : String
+                    var NP : String
+
+
+                    DC = doc.get("DiaConsulta").toString()
+                    NP = doc.get("EmaiPaciente").toString()
+                    NM = doc.get("EmailMedico").toString()
+                    HC = doc.get("HoraConsulta").toString()
+
+if(DC.toString().equals( tvconsultasdatepickertext.text)){
+
+                    var todoList = mutableListOf(Consultas(DC, NP, NM, HC))
 
 
 
 
-                        val adapter = MinhasConsultasAdapter(todoList)
-                        rvTODO.adapter = adapter
-                        rvTODO.layoutManager = LinearLayoutManager(this)
+                    val adapter = MinhasConsultasAdapter(todoList)
+                    rvTODO.adapter = adapter
+                    rvTODO.layoutManager = LinearLayoutManager(this)
 
 
 
 
-                    }
-                } else {
-                    Log.w(ChatLogActivity.TAG, "Error getting documents.", task.exception)
+}
+                    else{
+    Toast.makeText(this, "Nao existem consultas nesse Dia", Toast.LENGTH_SHORT).show()
+
+
+
+    Toast.makeText(this, "Nao existem consultas nesse Dia", Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, "Nao existem consultas nesse Dia", Toast.LENGTH_SHORT).show()
+
                 }
             }
 
+    }
+}
 
 
 
+
+        //fazer aqui cena das datas
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        btconsultasdatepicker.setOnClickListener {
+            val dpd = DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, mYear,mMonth,mDay ->
+                tvconsultasdatepickertext.setText(""+mYear+"/"+(mMonth+1)+"/"+mDay)
+
+
+            },year,month,day)
+            dpd.show()
+
+
+
+        }
+
+
+
+        ReceDia()
 
     }}
+
+
