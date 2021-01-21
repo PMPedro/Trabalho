524f4c3974254dde2aaae677a2002a121a881692
a18094ipca <a18094@alunos.ipca.pt>
2021-01-21 23:18:57 +0000

    .

--- app/src/main/java/com/example/healthyabj/MEDICOS/Medicos_Home.kt
+++ app/src/main/java/com/example/healthyabj/MEDICOS/Medicos_Home.kt
@@ -60,8 +60,8 @@ class Medicos_Home  : AppCompatActivity() {
                         if (email.toLowerCase() == useremail) {
 
                             tvmedicoshomepageEmail.setText(useremail)
-                         //   CALOUPORRA.setText(name)
-                            tvmedicosdate.setText(date)
+                            tvmedicoshomepageNome.setText(name)
+                            tvmedicosDate.setText(date)
 
 
                         }
